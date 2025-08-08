0a7d13eb159eea62b87366c0084d4284d14ead74
samuel150a <samuelofficial184@gmail.com>
2025-08-08 23:19:48 +0530

    My firstSpring-MVC

---
+++ Na-Geo-Wild/src/main/java/com/xworkz/wild/config/WildInitializer.java
@@ -0,0 +1,20 @@
+package com.xworkz.wild.config;
+
+public class WildInitializer extends AbstractAnnotationConfigDispatcherServletInitializer  {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
