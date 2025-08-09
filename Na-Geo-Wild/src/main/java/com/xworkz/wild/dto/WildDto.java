26712d75c5f20cf421e3380aacc01f368e15d4a2
samuel150a <samuelofficial184@gmail.com>
2025-08-09 17:53:20 +0530

    Spring-jpa

---
+++ Na-Geo-Wild/src/main/java/com/xworkz/wild/dto/WildDto.java
@@ -0,0 +1,13 @@
+package com.xworkz.wild.dto;
+
+import lombok.Data;
+
+@Data
+public class WildDto {
+    private String documentaryName;
+    private int episode;
+    private String photographer;
+    private String filmingLocation;
+    private String category;
+
+}
