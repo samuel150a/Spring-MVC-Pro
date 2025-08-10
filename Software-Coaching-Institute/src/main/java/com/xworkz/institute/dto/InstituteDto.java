359ff5ab9fbdb7b992ce83bd029d208151a96e23
samuel150a <samuelofficial184@gmail.com>
2025-08-10 23:02:51 +0530

    Spring - Jpa

---
+++ Software-Coaching-Institute/src/main/java/com/xworkz/institute/dto/InstituteDto.java
@@ -0,0 +1,12 @@
+package com.xworkz.institute.dto;
+
+import lombok.Data;
+
+@Data
+public class InstituteDto {
+    private String candidateName;
+    private String degree;
+    private String yearOfPassout;
+    private String course;
+    private Integer duration;
+}
