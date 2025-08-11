2337628415fe75968dff109ba00299afddd95b91
samuel150a <samuelofficial184@gmail.com>
2025-08-11 23:34:56 +0530

    Spring - Jpa

--- Software-Coaching-Institute/src/main/java/com/xworkz/institute/repository/InstituteRepository.java
+++ Software-Coaching-Institute/src/main/java/com/xworkz/institute/repository/InstituteRepository.java
@@ -3,6 +3,9 @@ package com.xworkz.institute.repository;
 import com.xworkz.institute.dto.InstituteDto;
 import com.xworkz.institute.entity.InstituteEntity;
 
+import java.util.List;
+
 public interface InstituteRepository {
     boolean saveDetails(InstituteEntity instituteEntity);
+    List<InstituteEntity> fethAllData();
 }
