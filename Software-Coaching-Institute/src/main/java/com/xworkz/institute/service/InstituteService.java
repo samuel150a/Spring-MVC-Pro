ca4e0ebfa5fb6cddabdda63f8265c02303aaa55b
samuel150a <samuelofficial184@gmail.com>
2025-08-11 23:35:49 +0530

    Spring - Jpa

--- Software-Coaching-Institute/src/main/java/com/xworkz/institute/service/InstituteService.java
+++ Software-Coaching-Institute/src/main/java/com/xworkz/institute/service/InstituteService.java
@@ -1,13 +1,16 @@
 package com.xworkz.institute.service;
 
 import com.xworkz.institute.dto.InstituteDto;
+import com.xworkz.institute.entity.InstituteEntity;
 import com.xworkz.institute.repository.InstituteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 
 public interface InstituteService {
 
     boolean saveDetails(InstituteDto institueDto);
-
+    List<InstituteDto> fethAllData();
 
 }
