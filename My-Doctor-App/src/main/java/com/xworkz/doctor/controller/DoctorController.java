977c5382d3d7ec13fc6163553ca2e682275d1314
samuel150a <samuelofficial184@gmail.com>
2025-08-11 23:39:07 +0530

    Spring - Jpa

--- My-Doctor-App/src/main/java/com/xworkz/doctor/controller/DoctorController.java
+++ My-Doctor-App/src/main/java/com/xworkz/doctor/controller/DoctorController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+
+
 public class DoctorController {
     @Autowired
     DoctorService doctorService;
