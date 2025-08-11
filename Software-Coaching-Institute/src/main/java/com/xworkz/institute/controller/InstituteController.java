480ba49c46b51ee7faacc4bab10404b7e78acddc
samuel150a <samuelofficial184@gmail.com>
2025-08-11 23:33:21 +0530

    Spring - Jpa

--- Software-Coaching-Institute/src/main/java/com/xworkz/institute/controller/InstituteController.java
+++ Software-Coaching-Institute/src/main/java/com/xworkz/institute/controller/InstituteController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.swing.*;
 import java.sql.SQLOutput;
+import java.util.List;
 
 @Controller
 public class InstituteController {
@@ -33,10 +34,13 @@ public class InstituteController {
         System.out.println("Running in the details ");
         System.out.println(instituteDto);
          Boolean check=instituteService.saveDetails(instituteDto);
+
          if(check)
          {
              model.addAttribute("mess","Save done");
              System.out.println("Saved Successfully");
+
+
              return "InstituteSuccess";
          }
          else {
@@ -47,5 +51,14 @@ public class InstituteController {
          }
 
 
+    }
+    @GetMapping("Fetch")
+    public String FetchAll(Model model)
+    {
+        List<InstituteDto> fetchedDataDto=instituteService.fethAllData();
+        System.out.println("here is the feteched value");
+        fetchedDataDto.forEach(System.out::println);
+        model.addAttribute("ref",fetchedDataDto);
+          return "InstituteData";
     }
 }
