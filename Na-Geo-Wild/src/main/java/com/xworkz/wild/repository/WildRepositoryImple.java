012e4e5041ec7a5a64f5ecfab8f459a376dffdc6
samuel150a <samuelofficial184@gmail.com>
2025-08-11 23:37:49 +0530

    Spring - Jpa

--- Na-Geo-Wild/src/main/java/com/xworkz/wild/repository/WildRepositoryImple.java
+++ Na-Geo-Wild/src/main/java/com/xworkz/wild/repository/WildRepositoryImple.java
@@ -6,37 +6,56 @@ import com.xworkz.wild.service.WildServiceImple;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
+import java.util.List;
+
 @Repository
 public class WildRepositoryImple  implements  WildRepository {
-    @Override
-    public boolean saveDetails( WildEntity wildEntity) {
 
-        EntityManagerFactory entityManagerFactory=null;
-        EntityManager entityManager=null;
-        EntityTransaction entityTransaction=null;
-        try{
-            entityManagerFactory= Persistence.createEntityManagerFactory("x-workz");
-            entityManager=entityManagerFactory.createEntityManager();
-            entityTransaction=entityManager.getTransaction();
+    EntityManager entityManager = null;
+    EntityTransaction entityTransaction = null;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("x-workz");
+
+    @Override
+    public boolean saveDetails(WildEntity wildEntity) {
+
+
+        try {
+            entityManager = emf.createEntityManager();
+            entityTransaction = entityManager.getTransaction();
 
             entityTransaction.begin();
             entityManager.persist(wildEntity);
             entityTransaction.commit();
 
-        }
-        catch(PersistenceException e){
+        } catch (PersistenceException e) {
             System.out.println(e.getMessage());
-        }
-        finally{
-            if(entityManagerFactory!=null)
-            {
-                entityManagerFactory.close();
+        } finally {
+            if (emf != null) {
+                emf.close();
             }
-            if (entityManager!=null)
-            {
+            if (entityManager != null) {
                 entityManager.close();
             }
         }
         return true;
+    }
+
+    @Override
+    public List<WildEntity> fetchListOfEntities() {
+        List<WildEntity> fetch = null;
+        try {
+            fetch = emf.createEntityManager().createNamedQuery("fetchAllWildEntity").getResultList();
+            //System.out.println(fetch);
+        } catch (PersistenceException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (emf != null) {
+                emf.close();
+            }
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+        return fetch;
     }
 }
