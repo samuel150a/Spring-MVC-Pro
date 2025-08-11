62ff6f704eb09fa779bfc38dc2ab06bef98446ea
samuel150a <samuelofficial184@gmail.com>
2025-08-11 23:38:41 +0530

    Spring - Jpa

--- Na-Geo-Wild/src/main/java/com/xworkz/wild/service/WildService.java
+++ Na-Geo-Wild/src/main/java/com/xworkz/wild/service/WildService.java
@@ -1,7 +1,12 @@
 package com.xworkz.wild.service;
 
 import com.xworkz.wild.dto.WildDto;
+import com.xworkz.wild.entity.WildEntity;
+
+import java.util.List;
 
 public interface WildService {
     boolean saveDetails(WildDto wildDto);
+    List<WildDto> fetchListOfEntities();
+
 }
