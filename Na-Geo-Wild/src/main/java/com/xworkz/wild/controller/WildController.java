b51fe93ed5d56f2c4bd93de47eba0b6281373cdf
samuel150a <samuelofficial184@gmail.com>
2025-08-12 20:00:38 +0530

    Spring-jpa

--- Na-Geo-Wild/src/main/java/com/xworkz/wild/controller/WildController.java
+++ Na-Geo-Wild/src/main/java/com/xworkz/wild/controller/WildController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.sql.SQLOutput;
 import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class WildController {
     @Autowired
     WildService wildService;
@@ -20,13 +23,13 @@ public class WildController {
         System.out.println("Running in the WildController Constructor ");
     }
 
-    @GetMapping("/click")
+    @GetMapping("click")
     public String RedirectPage() {
         System.out.println("Running in RedirectPage method ");
         return "Wild";
     }
 
-    @PostMapping("/details")
+    @PostMapping("details")
     public String fillDetails(WildDto wildDto, Model model) {
         System.out.println("Running in the fillDetails method ");
         System.out.println(wildDto);
@@ -35,8 +38,12 @@ public class WildController {
         boolean check = wildService.saveDetails(wildDto);
         if (check) {
             model.addAttribute("message", "Done");
+            List<WildDto> ref = wildService.fetchListOfEntities();
+            model.addAttribute("listOfDto", ref);
+            System.out.println("Final returned value");
+            ref.forEach(System.out::println);
 
-            return "WildSuccesfull";
+            return "WildEntities";
         } else {
             model.addAttribute("error", "Failed");
 
@@ -45,16 +52,42 @@ public class WildController {
         }
     }
 
-@GetMapping("/getAllData")
-        public String getAllData(Model model)
-        {
-            System.out.println("Running in the getAllData");
-             List<WildDto> ref=wildService.fetchListOfEntities();
-             model.addAttribute("listOfDto",ref);
-            System.out.println("Final returned value");
-            ref.forEach(System.out::println);
+    @GetMapping({"getAllData","wback"})
+    public String getAllData(Model model) {
+        System.out.println("Running in the getAllData");
+        List<WildDto> ref = wildService.fetchListOfEntities();
+        model.addAttribute("listOfDto", ref);
+        System.out.println("Final returned value");
+        ref.forEach(System.out::println);
 
-             return "WildEntities";
-        }
+        return "WildEntities";
+    }
+
+    @GetMapping("back")
+    public String indexPage() {
+        System.out.println("Running in the indexPage Method");
+        return "index";
+    }
+
+
+
+    @GetMapping("view/{id}")
+    public String getId(@PathVariable Integer id,Model model)
+    {
+        System.out.println("get by id");
+        WildDto byId=wildService.fetchById(id);
+
+        System.out.println("controller a fetched ID :  "+byId);
+
+        model.addAttribute("onlyId",byId);
+
+        return "DisplayId";
+    }
+//    @GetMapping("/wback")
+//    public String wildEntitesTablePage() {
+//        System.out.println("Running in the wildEntitesTablePage Method");
+//        return "WildEntities";
+//    }
+
 
 }
