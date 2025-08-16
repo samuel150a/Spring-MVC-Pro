6e1a2ef7c45e033d177870aa9f1d7f0ef683e4f5
samuel150a <samuelofficial184@gmail.com>
2025-08-16 23:08:27 +0530

    init commit

--- My-Doctor-App/src/main/java/com/xworkz/doctor/repository/DoctorRepoImple.java
+++ My-Doctor-App/src/main/java/com/xworkz/doctor/repository/DoctorRepoImple.java
@@ -5,15 +5,19 @@ import com.xworkz.doctor.entity.DoctorEntity;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
+import java.util.List;
+
 @Repository
 public class DoctorRepoImple implements DoctorRepository{
+   private  EntityManagerFactory entityManagerFactory= Persistence.createEntityManagerFactory("x-workz");
+
     @Override
     public boolean save(DoctorEntity doctorEntity ) {
-        EntityManagerFactory entityManagerFactory=null;
+
         EntityManager entityManager=null;
         EntityTransaction entityTransaction=null;
+
         try{
-            entityManagerFactory= Persistence.createEntityManagerFactory("x-workz");
             entityManager=entityManagerFactory.createEntityManager();
             entityTransaction=entityManager.getTransaction();
 
@@ -27,16 +31,77 @@ public class DoctorRepoImple implements DoctorRepository{
             System.out.println(e.getMessage());
         }
         finally{
-            if(entityManagerFactory!=null)
-            {
-                entityManagerFactory.close();
-            }
+
             if (entityManager!=null)
             {
                 entityManager.close();
             }
         }
         return true;
+    }
+
+    @Override
+    public List<DoctorEntity> fetchAllEntites() {
+        EntityManager entityManager=null;
+
+        System.out.println("Running in the fetchAllEntites method");
+        List<DoctorEntity> doctorEntity=null;
+
+        try{
+            entityManager=entityManagerFactory.createEntityManager();
+            Query query= entityManager.createNamedQuery("fethAllEntites");
+            doctorEntity= query.getResultList();
+
+
+        }
+        catch(
+                PersistenceException e){
+            System.out.println(e.getMessage());
+        }
+        finally{
+
+            if (entityManager!=null)
+            {
+                entityManager.close();
+            }
+        }
+
+        return doctorEntity;
+    }
+
+    @Override
+    public DoctorEntity fetchByID(Integer id) {
+        System.out.println("Running in the fetchByID");
+        DoctorEntity fetchByID=null;
+       EntityManager entityManager=null;
+        try{
+            entityManager=entityManagerFactory.createEntityManager();
+            fetchByID=entityManager.find(DoctorEntity.class,id);
+
+
+
+        }
+        catch(
+                PersistenceException e){
+            System.out.println(e.getMessage());
+        }
+        finally{
+
+            if (entityManager!=null)
+            {
+                entityManager.close();
+            }
+        }
+
+        return fetchByID;
+    }
+
+    public   void closeEntityFactory()
+    {
+        if(entityManagerFactory!=null&& entityManagerFactory.isOpen())
+        {
+            entityManagerFactory.close();
+        }
     }
 }
 
