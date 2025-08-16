5346f8837f4cab23844e9124eff109476b768c13
samuel150a <samuelofficial184@gmail.com>
2025-08-16 23:09:51 +0530

    init commit

--- My-Doctor-App/src/main/java/com/xworkz/doctor/service/DoctorServiceImpl.java
+++ My-Doctor-App/src/main/java/com/xworkz/doctor/service/DoctorServiceImpl.java
@@ -6,6 +6,10 @@ import com.xworkz.doctor.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class DoctorServiceImpl implements DoctorService{
     @Autowired
@@ -21,4 +25,39 @@ doctorEntity.setDisabilities(doctorDto.isDisabilities());
 
         return doctorRepository.save(doctorEntity);
     }
+
+    @Override
+    public List<DoctorDto> fetchAllEntites() {
+        System.out.println("Running  in the fethAllEntites method ");
+        List<DoctorDto> doctorDto1=null;
+        List<DoctorEntity> doctorEntity =doctorRepository.fetchAllEntites();
+        doctorDto1= doctorEntity.stream().map(e->{
+            DoctorDto doctorDto=new DoctorDto();
+            doctorDto.setID(e.getID());
+            doctorDto.setPatientName(e.getPatientName());
+            doctorDto.setAge(e.getAge());
+            doctorDto.setHowLong(e.getHowLong());
+            doctorDto.setSickness(e.getSickness());
+            doctorDto.setDisabilities(e.isDisabilities());
+            return doctorDto;
+        }).collect(Collectors.toList());
+        return doctorDto1;
+    }
+
+    @Override
+    public DoctorDto fetchByID(Integer id) {
+        DoctorEntity doctorEntity=doctorRepository.fetchByID(id);
+        DoctorDto doctorDto=new DoctorDto();
+
+        doctorDto.setID(doctorEntity.getID());
+        doctorDto.setPatientName(doctorEntity.getPatientName());
+        doctorDto.setSickness(doctorEntity.getSickness());
+        doctorDto.setAge(doctorEntity.getAge());
+        doctorDto.setHowLong(doctorEntity.getHowLong());
+        doctorDto.setDisabilities(doctorEntity.isDisabilities());
+        
+        return doctorDto;
+    }
+
+
 }
