8b9e3148eb3c1d3fdd9bc921b0d0542624845a5f
samuel150a <samuelofficial184@gmail.com>
2025-08-16 23:08:44 +0530

    init commit

--- My-Doctor-App/src/main/java/com/xworkz/doctor/repository/DoctorRepository.java
+++ My-Doctor-App/src/main/java/com/xworkz/doctor/repository/DoctorRepository.java
@@ -3,6 +3,10 @@ package com.xworkz.doctor.repository;
 import com.xworkz.doctor.dto.DoctorDto;
 import com.xworkz.doctor.entity.DoctorEntity;
 
+import java.util.List;
+
 public interface DoctorRepository {
     boolean save(DoctorEntity doctorEntity);
+    List<DoctorEntity> fetchAllEntites();
+    DoctorEntity fetchByID(Integer id);
 }
