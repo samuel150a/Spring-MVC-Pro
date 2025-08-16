155984c00e7afccac78315713687d519085c5b43
samuel150a <samuelofficial184@gmail.com>
2025-08-16 23:07:47 +0530

    init commit

--- My-Doctor-App/src/main/java/com/xworkz/doctor/controller/DoctorController.java
+++ My-Doctor-App/src/main/java/com/xworkz/doctor/controller/DoctorController.java
@@ -5,11 +5,12 @@ import com.xworkz.doctor.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
-
+@RequestMapping("/")
 
 public class DoctorController {
     @Autowired
@@ -35,6 +36,10 @@ public class DoctorController {
         {
             System.out.println("details saved successfully");
             model.addAttribute("mess","Save done!");
+
+            List<DoctorDto> fetched=doctorService.fetchAllEntites();
+            System.out.println("The fetched datas are------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>");
+            fetched.forEach(System.out::println);
             return "DoctorSuccess";
         }
         else {
@@ -43,5 +48,48 @@ public class DoctorController {
             return "Doctor";
         }
         }
+        @GetMapping({"fetchAllData","Gback"})
+    public String fetchAllData(Model model)
+        {
+            System.out.println("Running in the fetchAllData in getMapping");
+            List<DoctorDto> fetched=doctorService.fetchAllEntites();
+            System.out.println("The fetched datas are------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>");
+            fetched.forEach(System.out::println);
+            model.addAttribute("data",fetched);
+            return "AllData";
+        }
+
+        @GetMapping("view/{ID}")
+    public String viewById(@PathVariable Integer ID, Model model)
+        {
+            DoctorDto doctorDto=doctorService.fetchByID(ID);
+            System.out.println("Running in the viewById method");
+            model.addAttribute("viewMe",doctorDto);
+            return "ViewById";
+        }
+
+    @GetMapping("/back")
+    public String backButton()
+    {
+        System.out.println("Running in the backButton method");
+        return "index";
+    }
+
+    @GetMapping("/dback")
+    public String backButton2()
+    {
+        System.out.println("Running in the backButton2 method");
+        return "index";
+    }
+
+    @GetMapping("/Eback")
+    public String backButton3()
+    {
+        System.out.println("Running in the backButton3 method");
+        return "Doctor";
+    }
+
+
+
     }
 
