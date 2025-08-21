6a12c1895fd66b91c487c783a2675361c9a30d5f
samuel150a <samuelofficial184@gmail.com>
2025-08-21 22:24:51 +0530

    Spring MVC

---
+++ e-commerce/src/main/java/com/xworkz/ecommerce/config/AppConfig.java
@@ -0,0 +1,28 @@
+package com.xworkz.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("com.xworkz.ecommerce")
+@EnableWebMvc
+public class AppConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
