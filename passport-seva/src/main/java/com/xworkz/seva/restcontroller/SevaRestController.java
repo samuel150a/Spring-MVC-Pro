f0652170b012c6c249763a294c066767d9f2b458
samuel150a <samuelofficial184@gmail.com>
2025-08-21 22:34:39 +0530

    Spring MVC

--- passport-seva/src/main/java/com/xworkz/seva/restcontroller/SevaRestController.java
+++ passport-seva/src/main/java/com/xworkz/seva/restcontroller/SevaRestController.java
@@ -8,32 +8,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
+@RequestMapping("/")
+public class SevaRestController {
+    public SevaRestController() {
+        System.out.println("Running in the SevaRestController");
+    }
 
-    @RestController
-    @RequestMapping("/")
-    public class SevaRestController {
-public SevaRestController()
-{
-    System.out.println("Running in the SevaRestController");
-}
-        @Autowired
-        private SevaService sevaService;
+    @Autowired
+    private SevaService sevaService;
 
-        @GetMapping("checkemail")
-        public String checkEmail(@RequestParam("email") String email) {
-            System.out.println("Running in the RestController");
+    @GetMapping("checkemail")
+    public String checkEmail(@RequestParam("email") String email) {
+        System.out.println("Running in the RestController");
 
-            System.out.println("emsi == === "+email);
-            boolean exists = sevaService.isEmailAlreadyUsed(email);
-            if(exists)
-            {
-                System.out.println("email is exists");
-                return "email is exists";
-            }
-            else {
-                System.out.println("email id not exists");
-                   return "email id not exists";
-            }
+        System.out.println("emsi == === " + email);
+        boolean exists = sevaService.isEmailAlreadyUsed(email);
+        if (exists) {
+            System.out.println("email is exists");
+            return "email is exists";
+        } else {
+            System.out.println("email id not exists");
+            return "email id not exists";
         }
     }
+}
 
