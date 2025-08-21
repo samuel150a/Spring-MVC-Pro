766b5126c90dd05058db80a16110d336f731ee52
samuel150a <samuelofficial184@gmail.com>
2025-08-21 22:29:52 +0530

    Spring MVC

---
+++ Rapido-Taxi-Booking/src/main/java/com/xworkz/rapido/configuration/RapidoConfiguration.java
@@ -0,0 +1,37 @@
+package com.xworkz.rapido.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("com.xworkz.rapido")
+@EnableWebMvc
+public class RapidoConfiguration implements WebMvcConfigurer {
+    public RapidoConfiguration()
+    {
+        System.out.println("Running in the RapidoConfiguration Constructor");
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver()
+    {
+        System.out.println("Running in the method ");
+        InternalResourceViewResolver resourceViewResolver=new InternalResourceViewResolver();
+        resourceViewResolver.setPrefix("/");
+        resourceViewResolver.setSuffix(".jsp");
+        return resourceViewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
+
+}
