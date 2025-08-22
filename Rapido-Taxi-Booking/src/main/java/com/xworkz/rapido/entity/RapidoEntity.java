52e12427efa33a10192db762f68902ef3ad41ea8
samuel150a <samuelofficial184@gmail.com>
2025-08-22 21:48:23 +0530

    Spring MVC

--- Rapido-Taxi-Booking/src/main/java/com/xworkz/rapido/entity/RapidoEntity.java
+++ Rapido-Taxi-Booking/src/main/java/com/xworkz/rapido/entity/RapidoEntity.java
@@ -6,56 +6,43 @@ import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Table(name="rapidoDetails3")
+@NamedQueries({
+        @NamedQuery(name="getByEmail", query="SELECT a.email FROM RapidoEntity a WHERE a.email=:email"),
+        @NamedQuery(name="getByPhoneNumber", query="SELECT p.phoneNumber FROM RapidoEntity p WHERE p.phoneNumber=:phoneNumber")
+})
 @Data
-@Table(name="rapidoDetails")
-//@NamedQuery(name = "getByEmail",query = "select a.email from UserEntity a where a.email = :email and a.isPresent=true")
-@NamedQuery(name="getByEmail",query="SELECT a.email from RapidoEntity a WHERE a.email=:email")
-@NamedQuery(name="getByPhoneNumber",query="SELECT P.phoneNumber from RapidoEntity p WHERE P.phoneNumber=:phoneNumber")
 public class RapidoEntity {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="ID")
-    private  int ID;
-
-//    private int ID;
-//    private String name;
-//    private String email;
-//    private String phoneNumber;
-//    private LocalDate dob;
-//    private  String gender;
-//    private String state;
-//    private String address;
-//    private String password;
-//    private String confirmPassword;
-
+    private Integer ID;
 
     @Column(name="name")
-    private  String name;
+    private String name;
 
     @Column(name="email")
-    private  String email;
-
+    private String email;
 
     @Column(name="phoneNumber")
-    private  int phoneNumber;
+    private Long phoneNumber;
 
     @Column(name="dob")
-    private  LocalDate dob;
+    private String dob;
 
     @Column(name="gender")
-    private  String gender;
+    private String gender;
 
     @Column(name="state")
-    private  String state;
+    private String state;
 
     @Column(name="address")
-    private  String address;
+    private String address;
 
     @Column(name="password")
-    private  String password;
+    private String password;
 
     @Column(name="confirmPassword")
-    private  String confirmPassword;
-
+    private String confirmPassword;
 }
