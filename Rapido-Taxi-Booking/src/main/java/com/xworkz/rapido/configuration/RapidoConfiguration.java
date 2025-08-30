cee91306f30131f15904bf5979381b303c2de7ac
samuel150a <samuelofficial184@gmail.com>
2025-08-30 23:05:34 +0530

    Spring MVC

--- Rapido-Taxi-Booking/src/main/java/com/xworkz/rapido/configuration/RapidoConfiguration.java
+++ Rapido-Taxi-Booking/src/main/java/com/xworkz/rapido/configuration/RapidoConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -33,6 +35,8 @@ public class RapidoConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/images/**").addResourceLocations("/images/");
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:D:/SaveMe/");
     }
 
     @Bean
@@ -41,4 +45,11 @@ public class RapidoConfiguration implements WebMvcConfigurer {
         return new BCryptPasswordEncoder();
     }
 
+
+    @Bean
+    public MultipartResolver multipartResolver()
+    {
+        System.out.println("Running in the multipartResolver method ");
+        return new StandardServletMultipartResolver();
+    }
 }
