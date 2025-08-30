529aa4d9de7122a9b2e1d637f47d9606e8fe7509
samuel150a <samuelofficial184@gmail.com>
2025-08-30 23:06:12 +0530

    Spring MVC

--- Rapido-Taxi-Booking/src/main/java/com/xworkz/rapido/configuration/RapidoInitializer.java
+++ Rapido-Taxi-Booking/src/main/java/com/xworkz/rapido/configuration/RapidoInitializer.java
@@ -2,10 +2,15 @@ package com.xworkz.rapido.configuration;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+import java.io.File;
+
 public class RapidoInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    public RapidoInitializer(){
+    public RapidoInitializer() {
         System.out.println("Running in the RapidoInitializer constructor");
     }
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[0];
@@ -19,5 +24,21 @@ public class RapidoInitializer extends AbstractAnnotationConfigDispatcherServlet
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+
+
     }
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration)
+    {
+        File uploadDirectory=new File(System.getProperty("java.io.tmpdir"));
+        int maxUploadSizeInMb=5*1024*1024;
+        MultipartConfigElement multipartConfigElement=new MultipartConfigElement(uploadDirectory.getAbsolutePath()
+                ,maxUploadSizeInMb,maxUploadSizeInMb*2
+                ,maxUploadSizeInMb/2);
+
+        registration.setMultipartConfig(multipartConfigElement);
+
+
+    }
+
 }
